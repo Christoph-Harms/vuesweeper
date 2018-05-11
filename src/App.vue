af628d0aa23d93a10ec401bfc1f3cf5b295bda3d
Christoph Harms-Ensink <christoph.harms-ensink@plentymarkets.com>
2018-05-11 11:13:15 +0200

    Cell component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Cell />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import './assets/styles/main.css'
+import Cell from './components/Cell.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Cell
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Cell.vue
@@ -0,0 +1,48 @@
+<template>
+    <div
+            class="border-grey-lighter border-2 w-10 h-10 text-white text-2xl flex justify-center flex-col flex items-center"
+            :class="[revealed ? revealedClass : unrevealedClass]"
+    >
+        <p v-html="labelHtml"></p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Cell',
+
+        props: {
+            revealed: {
+                type: Boolean,
+                default: false,
+            },
+            isBomb: {
+                type: Boolean,
+                default: false,
+            },
+            label: {
+                type: Number,
+                default: null,
+            },
+            marked: {
+                type: Boolean,
+                default: true,
+            }
+        },
+
+        computed: {
+            labelHtml() {
+                if (this.marked) return "<i class=\"fas fa-exclamation-triangle\"></i>"
+
+                return this.label
+            }
+        },
+
+        data() {
+            return {
+                unrevealedClass: "bg-grey-dark",
+                revealedClass: "bg-grey-light",
+            }
+        },
+    }
+</script>
